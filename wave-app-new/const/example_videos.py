a9c4e73c011590ccd4a75c4d161a5a5737a91f44
chiararucco <chiararuccoo@gmail.com>
2022-02-23 22:42:00 +0100

    Video added

--- wave-app-new/const/example_videos.py
+++ wave-app-new/const/example_videos.py
@@ -6,5 +6,9 @@ example_videos = [
   {
     'path': os.path.join('.', 'data', 'videos', '1.mp4'),
     'label': 'Cam video with wildfire'
+  },
+  {
+    'path': os.path.join('.', 'data', 'videos', '2.mp4'),
+    'label': 'Cam video with barely visible smoke'
   }
 ]
--- wave-app-new/model/model_interface.py
+++ wave-app-new/model/model_interface.py
@@ -15,7 +15,7 @@ class ModelInference(object):
                                     'custom',
                                     path=model_checkpoint_path,
                                     force_reload=True)  # local repo
-        self.model.conf = 0.2
+        self.model.conf = 0.05
 
     def inference(self,
                   img: "numpy.ndarray") -> None:
