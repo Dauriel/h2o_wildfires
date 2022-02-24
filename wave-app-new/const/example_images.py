9a22da611b67c153764b84316861cd87d1e3956a
chiararucco <chiararuccoo@gmail.com>
2022-02-24 23:58:50 +0100

    example images

--- wave-app-new/const/example_images.py
+++ wave-app-new/const/example_images.py
@@ -13,10 +13,18 @@ example_images = [
   },
   {
     'path': os.path.join('.', 'data', 'images', '3.jpeg'),
-    'label': 'Cam image with smoke'
+    'label': 'Cam image with smoke 1 '
   },
   {
     'path': os.path.join('.', 'data', 'images', '4.jpeg'),
-    'label': 'Cam image with smoke'
+    'label': 'Cam image with smoke 2'
+  },
+  {
+    'path': os.path.join('.', 'data', 'images', '5.jpeg'),
+    'label': 'Cam image with far away smoke'
+  },
+  {
+    'path': os.path.join('.', 'data', 'images', '6.png'),
+    'label': 'Cam image with big smoke'
   }
 ]
